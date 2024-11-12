1b561e636aa1b634e2205f36573e73bb9385edf3
EnzoSur <enzosurhati@gmail.com>
2024-11-12 10:48:58 -0500

    batman

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ContactList from './Components/ContactList';
 import SelectedContact from './Components/SelectedContact';
+import './App.css';
 
 function App() {
   const [selectedContactId, setSelectedContactId] = useState(null);
